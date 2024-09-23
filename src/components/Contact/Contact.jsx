b3906aa37e01110bfe0d999f3baec4fbf0107e25
muteeb waheed <muteebwaheed6@gmail.com>
2024-09-22 20:09:08 -0700

    2nd commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,18 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="muteebwaheed6@gmail.com">GMail.com/muteebwaheed</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/muteeb-waheed-988b31288/">linkedin.com/muteebwaheed</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://github.com/muteedwaheed">github.com/muteebwaheed</a>
         </li>
       </ul>
     </footer>
